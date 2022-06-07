ff308a8c219a38e3ca8398c7e653b24d7024ac2a
Kurogoma939 <kan.sss.939@gmail.com>
2022-06-07 13:43:01 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/chat/ex/app/read/chat_app_read/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chat.ex.app.read.chat_app_read
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('サンプルSNS'),
+      ),
+      body: const Center(
+        child: Text('サンプル'),
+      ),
+    );
+  }
+}
